3035b575d435abbc1475c1b251b1216e24c351fa
bea7992 <beacampos127@gmal.com>
2021-03-28 22:30:49 +0200

    cambio puerto

--- dbConfig.js
+++ dbConfig.js
@@ -6,8 +6,8 @@ const connect = () => {
     const pool = mysql.createPool({
         host: '127.0.0.1',
         user: 'root',
-        password: '',
-        port: 3306,
+        password: 'root',
+        port: 8889,
         database: 'photoprodb'
     });
     global.db = pool;
